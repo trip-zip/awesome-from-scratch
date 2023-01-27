2866d768995bb22e67b7b415ecac354557b67ef7
Jimmy Cozza <moatcozza@gmail.com>
2023-01-26 21:04:25 -0700

    cleanup

--- rc.lua
+++ rc.lua
@@ -113,8 +113,6 @@ mytextclock = wibox.widget.textclock()
 
 -- @DOC_FOR_EACH_SCREEN@
 screen.connect_signal("request::desktop_decoration", function(s)
-  -- Each screen has its own tag table.
-  -- awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 
   -- Create a promptbox for each screen
   s.mypromptbox = awful.widget.prompt()
@@ -138,35 +136,7 @@ screen.connect_signal("request::desktop_decoration", function(s)
       end),
     },
   })
-
-  -- Create a taglist widget
-  -- s.mytaglist = awful.widget.taglist({
-  --   screen = s,
-  --   filter = awful.widget.taglist.filter.all,
-  --   buttons = {
-  --     awful.button({}, 1, function(t)
-  --       t:view_only()
-  --     end),
-  --     awful.button({ modkey }, 1, function(t)
-  --       if client.focus then
-  --         client.focus:move_to_tag(t)
-  --       end
-  --     end),
-  --     awful.button({}, 3, awful.tag.viewtoggle),
-  --     awful.button({ modkey }, 3, function(t)
-  --       if client.focus then
-  --         client.focus:toggle_tag(t)
-  --       end
-  --     end),
-  --     awful.button({}, 4, function(t)
-  --       awful.tag.viewprev(t.screen)
-  --     end),
-  --     awful.button({}, 5, function(t)
-  --       awful.tag.viewnext(t.screen)
-  --     end),
-  --   },
-  -- })
-
+  
   -- @TASKLIST_BUTTON@
   -- Create a tasklist widget
   s.mytasklist = awful.widget.tasklist({
